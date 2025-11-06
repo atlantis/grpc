e9005a248c5c0b18282c251c73c66578b48e54cd
atlantisnova <atlantisnova@gmail.com>
2025-11-06 12:29:10 -0800

    Add helpers to grpc encode/decode protobufs

--- src/grpc.cr
+++ src/grpc.cr
@@ -8,3 +8,28 @@ require "./client"
 require "./status_codes"
 require "./errors"
 require "./service"
+
+module GRPC
+  # grpc-encod a protobuf object by prefixing compression and length
+  def self.encode_protobuf(object : Protobuf::Message)
+    io = IO::Memory.new
+
+    request_payload = object.to_protobuf.to_slice
+
+    io.write_bytes(0_u8) # Not compressed
+    io.write_bytes(request_payload.size, IO::ByteFormat::BigEndian)
+    io.write request_payload
+
+    io.to_slice
+  end
+
+  # decode a grpc-encoded protobuf by stripping off compression and length prefix
+  def self.decode_protobuf(io, klass : Protobuf::Message.class)
+      compressed = io.read_byte != 0
+      size = io.read_bytes(Int32, IO::ByteFormat::BigEndian)
+      bytes = Bytes.new(size)
+      io.read(bytes)
+      payload = IO::Memory.new(bytes)
+      klass.from_protobuf(payload)
+  end
+end
